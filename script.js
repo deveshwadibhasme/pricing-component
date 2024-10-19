c0cfc86e486821854c125af6d77f224d91f26636
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-10-20 02:28:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
-let pageCount = document.querySelector('#count')
-let priceByMonth = document.querySelector('#price-value')
-let rangeInput = document.querySelector('.range-input')
-let toggleSwitch = document.querySelector('#toggle-switch')
+const pageCount = document.querySelector('#count');
+const priceByMonth = document.querySelector('#price-value');
+const rangeInput = document.querySelector('.range-input');
+const toggleSwitch = document.querySelector('#toggle-switch');
 
-rangeInput.addEventListener('input', function() {
+rangeInput.addEventListener('input', function () {
     const value = this.value;
     this.style.background = `linear-gradient(to right, cyan ${value}%, #ccc ${value}%)`;
 });
 
-let priceArray = [8,12,16,24,36]
-let discountedArray
+// Array of default prices
+const priceArray = [8, 12, 16, 24, 36];
 
-function discountToggle(valueChecked){
-    if(!valueChecked){
-        priceArray = [8,12,16,24,36]
-    }
-    else{
-        priceArray = priceArray.map(price=>'0'+(price*25)/100)
-    }
+
+// Function to toggle discount
+function applyDiscount(isChecked) {
+    return isChecked ? priceArray.map(price => (price*25)/100) : [...priceArray];
 }
 
-toggleSwitch.addEventListener('input',(toggle)=>{
-    discountToggle(toggle.target.checked)
-})
-
-
-pageCount.textContent = 0
-function updateValue(rangeValue){
-    // console.log(rangeValue)
-    switch(parseInt(rangeValue)){   
+// Function to update page views and price based on range input value
+function updatePricing(rangeValue, prices) {
+    switch (parseInt(rangeValue)) {
         case 20:
-            pageCount.textContent = '10K'
-            priceByMonth.textContent = `$${priceArray[0]}.00`
+            pageCount.textContent = '10K';
+            priceByMonth.textContent = `$${prices[0]}.00`;
             break;
         case 40:
-            pageCount.textContent = '50K'
-            priceByMonth.textContent = `$${priceArray[1]}.00`
+            pageCount.textContent = '50K';
+            priceByMonth.textContent = `$${prices[1]}.00`;
             break;
         case 60:
-            pageCount.textContent = '100K'
-            priceByMonth.textContent =`$${priceArray[2]}.00`
+            pageCount.textContent = '100K';
+            priceByMonth.textContent = `$${prices[2]}.00`;
             break;
         case 80:
-            pageCount.textContent = '500K'
-            priceByMonth.textContent = `$${priceArray[3]}.00`
+            pageCount.textContent = '500K';
+            priceByMonth.textContent = `$${prices[3]}.00`;
             break;
         case 100:
-            pageCount.textContent = '1M'
-            priceByMonth.textContent = `$${priceArray[4]}.00`
+            pageCount.textContent = '1M';
+            priceByMonth.textContent = `$${prices[4]}.00`;
             break;
         default:
-            pageCount.textContent = 0
-            priceByMonth.textContent = `$00.00`
-        }
+            pageCount.textContent = '0';
+            priceByMonth.textContent = `$00.00`;
+    }
 }
 
-rangeInput.addEventListener('input',(range)=>{
-    updateValue(range.target.value)
-})
+// Event listener for range input to update price and views
+rangeInput.addEventListener('input', function (event) {
+    const currentPrices = applyDiscount(toggleSwitch.checked);
+    updatePricing(event.target.value, currentPrices);
+});
+
+// Event listener for toggle switch to apply discount
+toggleSwitch.addEventListener('input', function (event) {
+    const currentPrices = applyDiscount(event.target.checked);
+    updatePricing(rangeInput.value, currentPrices);
+});
+
+// Initial setup to reset page views and price
+pageCount.textContent = '0';
+priceByMonth.textContent = `$00.00`;
